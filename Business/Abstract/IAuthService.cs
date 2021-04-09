9c42203d54e64f32e20596056140dc3266d46814
Kerem Candangil <keremc4306@gmail.com>
2021-04-09 11:18:07 +0300

    14. gün commit 2 JWT entegrasyonu yapıldı

--- Business/Abstract/IAuthService.cs
+++ Business/Abstract/IAuthService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Jwt;
+using Entities.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +10,9 @@ namespace Business.Abstract
 {
     public interface IAuthService
     {
-
+        IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
+        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+        IResult UserExists(string email);
+        IDataResult<AccessToken> CreateAccessToken(User user);
     }
 }
--- DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using Core.Entities.Concrete;
 using DataAccess.Abstract;
+using System.Linq;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,5 +11,18 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfUserDal : EfEntityRepositoryBase<User, CarRentalContext>, IUserDal
     {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new CarRentalContext())
+            {
+                var result = from operationClaim in context.OperationClaims
+                             join userOperationClaim in context.UserOperationClaims
+                                 on operationClaim.Id equals userOperationClaim.OperationClaimId
+                             where userOperationClaim.UserId == user.Id
+                             select new OperationClaim {Id = operationClaim.Id, Name = operationClaim.Name};
+                return result.ToList();
+
+            }
+        }
     }
 }
