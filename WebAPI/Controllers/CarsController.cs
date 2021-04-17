26cfd01b7ce59d17f2af56a8f6aa434ae3f9f507
Kerem Candangil <keremc4306@gmail.com>
2021-04-17 22:01:30 +0300

    WebAPI katmanındaki CarsController sınıfında değişiklikler yapıldı

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -96,5 +96,16 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+
+        [HttpGet("getbybrand")]
+        public IActionResult GetByBrandId(int brandId)
+        {
+            var result = _carService.GetCarsByBrandId(brandId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
     }
 }
